05da4c8a2606c8012dda27e158b9656472bd4969
meddhiaka <kassab@fsb.u-carthage.tn>
2023-03-02 23:54:22 +0100

    Meet the Team demo inside test Team Comp

--- src/UI/Sub/TeamSubComp.jsx
+++ src/UI/Sub/TeamSubComp.jsx
@@ -34,7 +34,7 @@ export default function TeamSubComp() {
               />
             </svg>
           </span>
-          Meet The Team
+          Meet The Team Demo :)
         </h2>
       </div>
       <div className='grid gap-10 sm:grid-cols-2 lg:grid-cols-4'>
