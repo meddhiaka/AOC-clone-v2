0d98018b7c751ce3226ac437ed67ad06c949ebc3
meddhiaka <kassab@fsb.u-carthage.tn>
2023-03-12 15:51:51 +0100

    Fetching Team Members  Component Sucess

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-import envCompatible from 'vite-plugin-env-compatible';
-
-dotenv.config();
 
 export default defineConfig({
-  plugins: [react(), envCompatible()],
+  plugins: [react()],
   server: {
     host: true,
   },
