43cbabcc72bb86582bbb2589619516c6f2266398
meddhiaka <kassab@fsb.u-carthage.tn>
2023-04-03 20:57:40 +0100

    Active Events Comp added Demo

--- src/UI/Events.jsx
+++ src/UI/Events.jsx
@@ -1,4 +1,5 @@
 import EventsSubComp from './Sub/EventsSubComp';
+import ActiveEvents from './Sub/ActiveEvents';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
@@ -6,6 +7,7 @@ export default function Events() {
   return (
     <div>
       <Navbar />
+      <ActiveEvents />
       <EventsSubComp />
       <Footer />
     </div>
--- src/UI/Sub/ActiveEvents.jsx
+++ src/UI/Sub/ActiveEvents.jsx
@@ -1,3 +1,13 @@
 export default function ActiveEvents() {
-  return <div>test</div>;
+  return (
+    <section className='text-gray-600 body-font bg-yellow-200'>
+      <div className='container px-5 py-24 mx-auto'>
+        <div className='max-w-xl mx-auto text-center'>
+          <h2 className='font-heading text-4xl md:text-4xl text-yellow-600 font-black tracking-tight'>
+            Coming Events ...
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 }
