826d7078caaecd2947aa118af544c7e52adaf4e9
Med Dhia Kassab <mohameddhiakb@gmail.com>
2023-06-13 19:02:24 +0100

    Sponsoring button optimized

--- src/UI/Sub/SponsorsSubComp.jsx
+++ src/UI/Sub/SponsorsSubComp.jsx
@@ -203,7 +203,9 @@ export default function SponsorsSubComp() {
                             </p>
                         </div>
                         <div class="flex flex-col items-center justify-center gap-3 mt-5 lg:flex-row lg:justify-center">
-                            <a class="inline-flex items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-gray-900 border-2 border-gray-900 rounded-full hover:bg-transparent hover:border-gray-900 hover:text-gray-900 focus:outline-none lg:w-auto" href="./../../ds.pdf">
+                            <a class="inline-flex items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-gray-900 border-2 border-gray-900 rounded-full hover:bg-transparent hover:border-gray-900 hover:text-gray-900 focus:outline-none lg:w-auto"
+                                href="./../../ds.pdf"
+                                target='_blank'>
                                 <span class="flex flex-col items-start ml-4 leading-none">
                                     <span class="mb-1 text-xs">Check out our</span>
                                     <span class="font-medium title-font">Sponsoring Folder</span>
